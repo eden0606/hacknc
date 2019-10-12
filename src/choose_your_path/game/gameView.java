6826ef6b084ce2832b51c751d90eb5f05f165f44
Eden Chou <edenchou@edens-air.wireless-1x.unc.edu>
2019-10-12 14:24:36 -0400

    gameview

---
+++ src/choose_your_path/game/gameView.java
@@ -0,0 +1,21 @@
+package choose_your_path.game;
+
+import javax.swing.JFrame;
+
+public class gameView {
+
+	public gameView() {
+	}
+		
+		public static void main(String[] args) {
+			JFrame mainFrame = new JFrame();
+			mainFrame.setTitle("hacknc 2019");
+			mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			//mainFrame.setContentPane();
+			
+			mainFrame.pack();
+			mainFrame.setVisible(true);
+		
+	}
+}
